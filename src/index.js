9be52b6d79cb480778692a4534129eb570a62e72
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-07 16:06:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+  function del(con){
+    let n=document.getElementById(con);
+    n.parentElement.remove();
+
+   }
+   document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+ let tasks=document.getElementById("tasks");
+ let counter=0;
+function handleClick(e) {
+
+ const text=document.getElementById("new-task-description").value;
+ const s=document.createElement("li");
+
+ tasks.appendChild(s);
+ s.innerHTML=text + "<button onclick='del("+counter+")' id='"+counter+"'>X</button>";
+ console.log(text);
+ counter++;
+ event.preventDefault();
+
+}
+
+let Submit=document.querySelector("input[type='submit']");
+Submit.addEventListener("click", handleClick);
+
+
 });
